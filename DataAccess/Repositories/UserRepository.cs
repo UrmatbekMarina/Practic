90eb0ce8ca5bc5c1bf57d42cf63ac23328417b4a
UrmatbekMarina <125025135+UrmatbekMarina@users.noreply.github.com>
2023-04-10 13:36:19 +0300

    4.3

---
+++ DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Interfaces;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+
+        public UserRepository(KITSUNContext repositoryContext)
+            : base(repositoryContext)
+        { }
+    }
+}
---
+++ DataAccess/Wrapper/RepositoryWrapper.cs
@@ -0,0 +1,38 @@
+﻿//using DataAccess.Interfaces;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Wrapper;
+
+namespace DataAccess.Wrapper
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private KITSUNContext _repoContext;
+        private IUserRepository _user;
+        public IUserRepository User
+        {
+            get
+            {
+                if (_user == null)
+                {
+                    _user = new UserRepository(_repoContext);
+                }
+                return _user;
+            }
+        }
+        public RepositoryWrapper(KITSUNContext repositoryContext)
+        {
+            _repoContext = repositoryContext;
+        }
+
+        public async Task Save()
+        {
+            await _repoContext.SaveChangesAsync();
+        }
+    }
+}
