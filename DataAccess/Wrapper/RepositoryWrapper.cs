c3c6bb505e5cc70c0163d6f6fe7ae5c65387e840
UrmatbekMarina <125025135+UrmatbekMarina@users.noreply.github.com>
2023-04-10 13:31:31 +0300

    4.2

---
+++ DataAccess/Wrapper/RepositoryWrapper.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Wrapper
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private KITSUNEEContext _repoContext;
+        private IUserRepository _user;
+        public IUserRepository User
+        {
+            get
+            {
+                if (_user == null)
+                {
+                    _user = new UserRepository(_repoContext);
+                }
+                return _user;
+            }
+        }
+        public RepositoryWrapper(KITSUNEEContext repositoryContext)
+        {
+            _repoContext = repositoryContext;
+        }
+
+        public void Save()
+        {
+            _repoContext.SaveChanges();
+        }
+    }
+}
