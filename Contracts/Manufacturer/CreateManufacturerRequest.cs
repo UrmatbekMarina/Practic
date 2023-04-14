bdaa225964ea4de95d733fd29fa0a9052258e16d
UrmatbekMarina <125025135+UrmatbekMarina@users.noreply.github.com>
2023-04-15 01:58:43 +0300

    Task 5(BackendApi/Contracts)

---
+++ Contracts/Manufacturer/CreateManufacturerRequest.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Domain.Models;
+using Domain.Interfaces;
+
+
+namespace BackendAPI.Contracts
+{
+    public class CreateManufacturerRequest
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Inn { get; set; }
+        public string? Location { get; set; }
+
+
+    }
+
+}
---
+++ Contracts/User/GetUserResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendAPI.Contracts.User
+{
+    public class GetUserResponse
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = null!;
+        public string UserRole { get; set; } = null!;
+        public string UserPassword { get; set; } = null!;
+        public string UserAddress { get; set; } = null!;
+
+    }
+
+}
