d846d8e77f0dfa82425db870cbc43a0d17d6d462
UrmatbekMarina <125025135+UrmatbekMarina@users.noreply.github.com>
2023-04-15 01:45:19 +0300

    5

---
+++ BusinessLogic/Services/CustomerService.cs
@@ -0,0 +1,53 @@
+﻿//using BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Domain.Interfaces;
+//using DataAccess.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+//using Domain.Wrapper;
+
+namespace BusinessLogic.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+        public CustomerService(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+        public async Task<List<Customer>> GetAll()
+        {
+            return await _repositoryWrapper.Customer.FindAll();
+        }
+
+
+        public async Task<Customer> GetById(int id)
+        {
+            var customer = await _repositoryWrapper.Customer
+            .FindByCondition(x => x.Id == id);
+            return customer.First();
+        }
+        public async Task Create(Customer model)
+        {
+            await _repositoryWrapper.Customer.Create(model);
+            _repositoryWrapper.Save();
+        }
+        public async Task Update(Customer model)
+        {
+            _repositoryWrapper.Customer.Update(model);
+            _repositoryWrapper.Save();
+        }
+        public async Task Delete(int id)
+        {
+            var customer = await _repositoryWrapper.Customer
+            .FindByCondition(x => x.Id == id);
+            _repositoryWrapper.Customer.Delete(customer.First());
+            _repositoryWrapper.Save();
+        }
+    }
+}
---
+++ BusinessLogic/Services/ManufacturerService.cs
@@ -0,0 +1,53 @@
+﻿//using BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Domain.Interfaces;
+//using DataAccess.Wrapper;
+using Microsoft.EntityFrameworkCore;
+
+//using Domain.Wrapper;
+
+namespace BusinessLogic.Services
+{
+    public class ManufacturerService : IManufacturerService
+    {
+        private IRepositoryWrapper _repositoryWrapper;
+        public ManufacturerService(IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+        public async Task<List<Manufacturer>> GetAll()
+        {
+            return await _repositoryWrapper.Manufacturer.FindAll();
+        }
+
+
+        public async Task<Manufacturer> GetById(int id)
+        {
+            var manufacturer = await _repositoryWrapper.Manufacturer
+            .FindByCondition(x => x.Id == id);
+            return manufacturer.First();
+        }
+        public async Task Create(Manufacturer model)
+        {
+            await _repositoryWrapper.Manufacturer.Create(model);
+            _repositoryWrapper.Save();
+        }
+        public async Task Update(Manufacturer model)
+        {
+            _repositoryWrapper.Manufacturer.Update(model);
+            _repositoryWrapper.Save();
+        }
+        public async Task Delete(int id)
+        {
+            var manufacturer = await _repositoryWrapper.Manufacturer
+            .FindByCondition(x => x.Id == id);
+            _repositoryWrapper.Manufacturer.Delete(manufacturer.First());
+            _repositoryWrapper.Save();
+        }
+    }
+}
